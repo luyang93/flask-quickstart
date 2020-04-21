18179b45a9c1999d9d6f09b80a6ceff825fdd9e9
luyang93 <510426762@qq.com>
2020-04-22 03:04:10 +0800

    URL Building

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -7,41 +7,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return 'index'
 
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/login')
+def login():
+    return 'login'
 
 
 @app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % escape(username)
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
 
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return 'Subpath %s' % escape(subpath)
-
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-
-@app.route('/about')
-def about():
-    return 'The about page'
-
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
