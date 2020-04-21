e559144cddb150330a6acbc50f09d39921a18947
luyang93 <510426762@qq.com>
2020-04-22 03:33:05 +0800

    Context Locals

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, url_for, request, render_template ,Markup
+from flask import Flask, url_for, request, render_template, Markup
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -36,6 +36,12 @@ with app.test_request_context():
     print(url_for('profile', username='John Doe'))
     print(url_for('static', filename='style.css')
           )
+
+with app.test_request_context('/hello', method='POST'):
+    # now you can do something with the request until the
+    # end of the with block, such as basic assertions:
+    assert request.path == '/hello'
+    assert request.method == 'POST'
 
 if __name__ == '__main__':
     Markup('<strong>Hello %s!</strong>') % '<blink>hacker</blink>'
