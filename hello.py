730264776956df6194af23f7ab2ea0ff2d273837
luyang93 <510426762@qq.com>
2020-04-22 11:10:46 +0800

    File Uploads

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, url_for, request, render_template, Markup
+import os
+
+from flask import Flask, url_for, request, render_template, Markup, flash, redirect
 from markupsafe import escape
 
 app = Flask(__name__)
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
@@ -41,13 +46,46 @@ def profile(username):
     return '{}\'s profile'.format(escape(username))
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = str(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
 with app.test_request_context():
     print(url_for('index'))
     print(url_for('login'))
-    print(url_for('login', next='/'))
     print(url_for('profile', username='John Doe'))
-    print(url_for('static', filename='style.css')
-          )
+    print(url_for('static', filename='style.css'))
+    print(url_for('static', filename='style.css'))
 
 with app.test_request_context('/hello', method='POST'):
     # now you can do something with the request until the
