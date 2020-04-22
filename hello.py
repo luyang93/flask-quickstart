760469d1e1a909c6aa07f8a747e0ca95cf82911c
luyang93 <510426762@qq.com>
2020-04-22 11:19:53 +0800

    Cookies

--- hello.py
+++ hello.py
@@ -1,100 +1,23 @@
 # -*- coding: utf-8 -*-
-import os
 
-from flask import Flask, url_for, request, render_template, Markup, flash, redirect
-from markupsafe import escape
+from flask import Flask, request, render_template, make_response
 
 app = Flask(__name__)
-UPLOAD_FOLDER = '/tmp'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
 def index():
-    return 'index'
+    username = request.cookies.get('username')
+    # use cookies.get(key) instead of cookies[key] to not get a
+    # KeyError if the cookie is missing.
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/')
+def index():
+    resp = make_response('sdjflksd')
+    resp.set_cookie('username', 'the username')
+    return resp
 
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = None
-    # searchword = request.args.get('key', '')
-    # if request.method == 'POST':
-    #     if valid_login(request.form['username'],
-    #                    request.form['password']):
-    #         return log_the_user_in(request.form['username'])
-    #     else:
-    #         error = 'Invalid username/password'
-    if request.method == 'POST':
-        if 0:
-            error = 'True'
-        else:
-            error = 'False'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
-
-
-@app.route('/user/<username>')
-def profile(username):
-    return '{}\'s profile'.format(escape(username))
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = str(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
-
-
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('profile', username='John Doe'))
-    print(url_for('static', filename='style.css'))
-    print(url_for('static', filename='style.css'))
-
-with app.test_request_context('/hello', method='POST'):
-    # now you can do something with the request until the
-    # end of the with block, such as basic assertions:
-    assert request.path == '/hello'
-    assert request.method == 'POST'
 
 if __name__ == '__main__':
-    Markup('<strong>Hello %s!</strong>') % '<blink>hacker</blink>'
-    Markup.escape('<blink>hacker</blink>')
-    Markup('<em>Marked up</em> &raquo; HTML').striptags()
     app.run(host='0.0.0.0', port=5000)
