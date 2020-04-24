c5f9b986daad5faf9788bd33f918fd8d612cc67c
luyang93 <510426762@qq.com>
2020-04-24 13:43:06 +0800

    Authentication

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 
 import pytest
+
 from flaskr import create_app
 from flaskr.db import get_db, init_db
 
@@ -36,3 +37,22 @@ def client(app):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
+
+
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
